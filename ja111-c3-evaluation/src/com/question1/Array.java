be0b6c66e9afd99ea7cab9f5cf828f57e9582602
shivamgarg796 <shivam.maheshwari796@gmail.com>
2022-06-06 16:09:37 +0530

    C3 evaluation

--- ja111-c3-evaluation/src/com/question1/Array.java
+++ ja111-c3-evaluation/src/com/question1/Array.java
@@ -12,5 +12,20 @@ public class Array {
 		int []arr=new int[size];
 		
 		System.out.println("Enter the elements in the array ");
+		for(int i=0;i<size;i++) {
+			System.out.println("Enter the" +" "+(i+1)+" "+ "Element");
+			arr[i]=sc.nextInt();	
+			}
+		
+//		System.out.println("After entering the elements of the array");
+//		for(int i=0;i<size;i++) {
+//			System.out.println(arr[i]);
+//			}
+		
+		System.out.println("Enter the index of the array element you want to access");
+		int index=sc.nextInt();
+			System.out.println("The array element at index "+index+ " "+arr[index]);
+					System.out.println("The array element successfully accessed");
+		
 	}
 }
