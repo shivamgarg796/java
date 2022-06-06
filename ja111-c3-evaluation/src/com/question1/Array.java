963e4df9960e1c4d723ec465fcec32ea44f3487c
shivamgarg796 <shivam.maheshwari796@gmail.com>
2022-06-06 16:12:16 +0530

    C3 evaluation

--- ja111-c3-evaluation/src/com/question1/Array.java
+++ ja111-c3-evaluation/src/com/question1/Array.java
@@ -17,15 +17,14 @@ public class Array {
 			arr[i]=sc.nextInt();	
 			}
 		
-//		System.out.println("After entering the elements of the array");
-//		for(int i=0;i<size;i++) {
-//			System.out.println(arr[i]);
-//			}
-		
 		System.out.println("Enter the index of the array element you want to access");
 		int index=sc.nextInt();
+		try {
 			System.out.println("The array element at index "+index+ " "+arr[index]);
 					System.out.println("The array element successfully accessed");
-		
+		}
+		catch(Exception e) {
+			System.out.println("you entered wrong index");
+		}
 	}
 }
